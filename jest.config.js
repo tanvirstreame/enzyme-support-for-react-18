d544c9b782299a4d03985436719e8011e5521eca
tanvirstreame <engr.tanvir.islam.1@gmail.com>
2024-07-18 10:36:17 +0600

    Add jest config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  };
